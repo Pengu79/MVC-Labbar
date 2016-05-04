1a10f160d57085ad738618d2d543ca4eeab1dc63
Pengu79 <gorbaz99@gmail.com>
2016-05-04 02:36:16 +0200

    Skapat lab2

---
+++ Lab2/Controllers/GamesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab2.Controllers
+{
+    public class GamesController : Controller
+    {
+        // GET: Games
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult TwentyOne()
+        {
+            return View();
+        }
+    }
+}
